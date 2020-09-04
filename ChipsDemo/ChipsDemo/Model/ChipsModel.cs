39917f9c2edb6c00a3e1037f7def505c8aca8f47
Chetan Shingala <Chetanshingala99@gmail.com>
2020-09-05 01:51:41 +0530

    Chips demo latest code

---
+++ ChipsDemo/ChipsDemo/Model/ChipsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Prism.Mvvm;
+
+namespace ChipsDemo.Model
+{
+    public class ChipsModel : BindableBase
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { SetProperty(ref _isSelected, value); }
+        }
+
+    }
+}
---
+++ ChipsDemo/ChipsDemo/ViewModels/ChipsPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using ChipsDemo.Model;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace ChipsDemo.ViewModels
+{
+    public class ChipsPageViewModel : ViewModelBase
+    {
+        public ChipsPageViewModel(INavigationService navigationPage) : base(navigationPage)
+        {
+        }
+
+        private ObservableCollection<ChipsModel> _itemCollection = new ObservableCollection<ChipsModel>();
+        public ObservableCollection<ChipsModel> ItemCollection
+        {
+            get { return _itemCollection; }
+            set { SetProperty(ref _itemCollection, value); }
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            ItemCollection = new ObservableCollection<ChipsModel>();
+            ItemCollection.Add(new ChipsModel() { Id = 1, Title = "Google", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 2, Title = "Yahoo message", IsSelected = true });
+            ItemCollection.Add(new ChipsModel() { Id = 3, Title = "Google doc", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 4, Title = "Miscrosoft", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 5, Title = "Google Pay", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 6, Title = "MSFT", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 7, Title = "Amazon", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 8, Title = "FlipCart", IsSelected = false });
+            ItemCollection.Add(new ChipsModel() { Id = 9, Title = "Amozon pay", IsSelected = false });
+        }
+
+        public DelegateCommand<ChipsModel> ChipClickCommand
+        {
+            get { return new DelegateCommand<ChipsModel>((obj) => OnChipClickCommandExecuted(obj)); }
+        }
+
+        private void OnChipClickCommandExecuted(ChipsModel item)
+        {
+            ItemCollection.All(arg =>
+            {
+                if (arg.Id == item.Id)
+                {
+                    if (arg.IsSelected)
+                        arg.IsSelected = false;
+                    else
+                        arg.IsSelected = true;
+                }
+                return true;
+            });
+        }
+
+
+    }
+}
